4f92f7de571191c81ae09683656fdfe95a5d5301
abhaysinghtomar97 <abhaysinghtomar97@gmail.com>
2025-08-12 01:06:06 +0530

    Template , Ui

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db =SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno =db.Column(db.Integer,primary_key=True)
+    title =db.Column(db.String,primary_key=False)
+    desc=db.Column(db.String,primary_key=False)
+    date_created =db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self):
+        return F"{self.sno}-{self.title}"
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello,Abhay Singh Tomar !</p>"
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
---
+++ reflection.md
@@ -0,0 +1,5 @@
+render_template('index.html') --> to render html by "import flask from render_template
+
+to activate --> .\env\Scripts\activate.ps1
+
+sql-alchemy --> facilitate to manage database through python -pip install flask-sqlalchemy       
