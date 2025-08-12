c89e70b89d75be1e7b21ce460d79a05a3ef9f610
abhaysinghtomar97 <abhaysinghtomar97@gmail.com>
2025-08-12 19:30:34 +0530

    Deletion of Task done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -7,6 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db =SQLAlchemy(app)
 
+# DataBase 
 class Todo(db.Model):
     sno =db.Column(db.Integer,primary_key=True)
     title =db.Column(db.String,primary_key=False)
@@ -17,16 +18,43 @@ class Todo(db.Model):
         return F"{self.sno}-{self.title}"
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
-    todo=Todo(title="First Job", desc="Start Investing in Stock MArket")
-    db.session.add(todo)
-    db.session.commit()
-    return render_template('index.html')
+    if request.method == 'POST':
+        title =request.form['title']
+        desc = request.form['desc']
+
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    
+    allTodo = Todo.query.all()
+    return render_template('index.html', allTodo=allTodo)
+
 
 @app.route("/about")
 def about():
     return "<p>I'm Btech Student , pursuing from PSIT Kanpur Looking Forward to Explore New technologies and Much more!</p>"
+
+
+@app.route('/show')
+def products():
+    allTodo =Todo.query.all()
+    print(allTodo)
+    return 'This is All Todos'
+
+@app.route('/update')
+def update():
+    allTodo =Todo.query.all()
+    print(allTodo)
+    return 'This is All Todos'
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo =Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ =="__main__":
     app.run(debug=True,port=8000)
--- reflection.md
+++ reflection.md
@@ -3,3 +3,23 @@ render_template('index.html') --> to render html by "import flask from render_te
 to activate --> .\env\Scripts\activate.ps1
 
 sql-alchemy --> facilitate to manage database through python -pip install flask-sqlalchemy       
+
+SQL lite Viewer --> to read database files
+
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db" => to add DB file in Folder
+
+jinja --> to 
+
+<form action="/" method="POST"  class="p-5"> -- >used to GET or POST data
+
+to Create New DB File :- 
+        >>python
+        >>from app import app, db
+        >>> with app.app_context():
+        ...     db.create_all()
+        ...
+
+ app.run(debug=True,port=8000) 
+
+    if debug =True == >Exact Error Will Show  => for developers
+    if debug =False ==> Random "Internal Error" => for users
